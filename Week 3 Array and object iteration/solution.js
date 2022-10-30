69e93af797ccafc4f6e61b59a6817d61b418d9ca
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2022-10-30 13:27:49 +0300

    Assignment files
    Readme and some basic code to start with.

---
+++ Week 3 Array and object iteration/solution.js
@@ -0,0 +1,24 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+function logDiary(array) {
+    
+}
+// Task 2
+
+const animal = {
+    canJump: true
+};
+
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+function birdCan(bird) {
+    
+}
+
+// Task 3
+
+function animalCan(animal) {
+    
+}
