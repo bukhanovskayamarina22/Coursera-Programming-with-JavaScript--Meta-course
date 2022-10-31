21613ff8036bc47f6677bd8fae29cc301941ce5c
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2022-10-31 18:36:10 +0300

    Completed
    Grade 100%

--- Week 3 Array and object iteration/solution.js
+++ Week 3 Array and object iteration/solution.js
@@ -1,24 +1,42 @@
 // Task 1
-var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
-function logDiary(array) {
-    
+var dairy = [
+  "cheese",
+  "sour cream",
+  "milk",
+  "yogurt",
+  "ice cream",
+  "milkshake",
+];
+
+function logDairy() {
+  for (const element of dairy) {
+    console.log(element);
+  }
 }
+
+console.log(logDairy(dairy));
+
 // Task 2
 
 const animal = {
-    canJump: true
+  canJump: true,
 };
 
 const bird = Object.create(animal);
 bird.canFly = true;
 bird.hasFeathers = true;
 
-function birdCan(bird) {
-    
+function birdCan() {
+  for (keys of Object.keys(bird)) console.log(keys, ": ", bird[keys]);
 }
+
+birdCan();
 
 // Task 3
 
-function animalCan(animal) {
-    
+function animalCan() {
+  for (prop in bird) {
+    console.log(prop, ": ", bird[prop]);
+  }
 }
+animalCan();
