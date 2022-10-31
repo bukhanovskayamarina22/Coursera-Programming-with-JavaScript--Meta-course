cea215b5e66569e041a2f13c5f0af1d6db0ea847
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2022-10-31 19:22:10 +0300

    Completed week 4 unit testing
    Graded 100%

--- Week 4 Unit testing/jest-testing/timesTwo.js
+++ Week 4 Unit testing/jest-testing/timesTwo.js
@@ -1,3 +1,7 @@
 // Task 1: Code the timesTwo function declaration
+function timesTwo(number) {
+    return number * 2;
+}
 
 // Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
